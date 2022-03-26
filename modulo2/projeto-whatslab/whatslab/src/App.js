00d156b42d66f4428ac0f6f98d81cd3635b485c1
mariabelencaldez <mariabelencaldez@gmail.com>
2022-03-25 22:43:33 -0300

    whatslab

---
+++ modulo2/projeto-whatslab/whatslab/src/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import './App.css';
+import ComponenteUm from "./Components/ComponenteUm";
+
+
+class App extends React.Component {
+  // state = {
+    
+  // };
+
+
+
+  // onChangeMensagem = (event) => {
+  //   this.setState({ mensagem: event.target.value });
+  // };
+
+ 
+  // onClickInput = () => {
+  //   const novaMensagem = {
+  //     id: this.state.id,
+  //     mensagem: this.state.mensagem,
+  //   };
+
+
+
+
+
+
+  render() {
+
+
+
+    return (
+      <div>
+        <div>
+          b
+        b
+        probando
+
+        <ComponenteUm/>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
